d1b377e417ba86d4d52f5484de08e367be81c7fa
David Burks <chesburks@gmail.com>
2024-08-21 21:18:05 -0400

    Add SM83 instruction names

--- emulator/include/cpu/cpu.h
+++ emulator/include/cpu/cpu.h
@@ -464,5 +464,47 @@ struct ARM7TDMI
 /// </summary>
 struct SM83
 {
-
+	void asm_adc();
+	void asm_add();
+	void asm_and();
+	void asm_bit();
+	void asm_call();
+	void asm_ccf();
+	void asm_cp();
+	void asm_cpl();
+	void asm_daa();
+	void asm_dec();
+	void asm_di();
+	void asm_ei();
+	void asm_halt();
+	void asm_inc();
+	void asm_jp();
+	void asm_jr();
+	void asm_ld();
+	void asm_ldh();
+	void asm_nop();
+	void asm_or();
+	void asm_pop();
+	void asm_push();
+	void asm_res();
+	void asm_ret();
+	void asm_reti();
+	void asm_rl();
+	void asm_rla();
+	void asm_rlc();
+	void asm_rlca();
+	void asm_rr();
+	void asm_rra();
+	void asm_rrc();
+	void asm_rst();
+	void asm_sbc();
+	void asm_scf();
+	void asm_set();
+	void asm_sla();
+	void asm_sra();
+	void asm_srl();
+	void asm_stop();
+	void asm_sub();
+	void asm_swap();
+	void asm_xor();
 };
