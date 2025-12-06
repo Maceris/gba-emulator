7dcd92eba5f805b8afb6d140d3c518fd9268c1d6
David Burks <chesburks@gmail.com>
2025-12-05 21:15:37 -0500

    Update SM83 function names

--- emulator/src/cpu/sm83.cpp
+++ emulator/src/cpu/sm83.cpp
@@ -1,133 +1,133 @@
 #include "cpu/sm83.h"
 
 #pragma region SM83 Instructions
-void SM83::asm_adc()
+void SM83::sm83_adc()
 {}
 
-void SM83::asm_add()
+void SM83::sm83_add()
 {}
 
-void SM83::asm_and()
+void SM83::sm83_and()
 {}
 
-void SM83::asm_bit()
+void SM83::sm83_bit()
 {}
 
-void SM83::asm_call()
+void SM83::sm83_call()
 {}
 
-void SM83::asm_ccf()
+void SM83::sm83_ccf()
 {}
 
-void SM83::asm_cp()
+void SM83::sm83_cp()
 {}
 
-void SM83::asm_cpl()
+void SM83::sm83_cpl()
 {}
 
-void SM83::asm_daa()
+void SM83::sm83_daa()
 {}
 
-void SM83::asm_dec()
+void SM83::sm83_dec()
 {}
 
-void SM83::asm_di()
+void SM83::sm83_di()
 {}
 
-void SM83::asm_ei()
+void SM83::sm83_ei()
 {}
 
-void SM83::asm_halt()
+void SM83::sm83_halt()
 {}
 
-void SM83::asm_inc()
+void SM83::sm83_inc()
 {}
 
-void SM83::asm_jp()
+void SM83::sm83_jp()
 {}
 
-void SM83::asm_jr()
+void SM83::sm83_jr()
 {}
 
-void SM83::asm_ld()
+void SM83::sm83_ld()
 {}
 
-void SM83::asm_ldh()
+void SM83::sm83_ldh()
 {}
 
-void SM83::asm_nop()
+void SM83::sm83_nop()
 {}
 
-void SM83::asm_or()
+void SM83::sm83_or()
 {}
 
-void SM83::asm_pop()
+void SM83::sm83_pop()
 {}
 
-void SM83::asm_push()
+void SM83::sm83_push()
 {}
 
-void SM83::asm_res()
+void SM83::sm83_res()
 {}
 
-void SM83::asm_ret()
+void SM83::sm83_ret()
 {}
 
-void SM83::asm_reti()
+void SM83::sm83_reti()
 {}
 
-void SM83::asm_rl()
+void SM83::sm83_rl()
 {}
 
-void SM83::asm_rla()
+void SM83::sm83_rla()
 {}
 
-void SM83::asm_rlc()
+void SM83::sm83_rlc()
 {}
 
-void SM83::asm_rlca()
+void SM83::sm83_rlca()
 {}
 
-void SM83::asm_rr()
+void SM83::sm83_rr()
 {}
 
-void SM83::asm_rra()
+void SM83::sm83_rra()
 {}
 
-void SM83::asm_rrc()
+void SM83::sm83_rrc()
 {}
 
-void SM83::asm_rst()
+void SM83::sm83_rst()
 {}
 
-void SM83::asm_sbc()
+void SM83::sm83_sbc()
 {}
 
-void SM83::asm_scf()
+void SM83::sm83_scf()
 {}
 
-void SM83::asm_set()
+void SM83::sm83_set()
 {}
 
-void SM83::asm_sla()
+void SM83::sm83_sla()
 {}
 
-void SM83::asm_sra()
+void SM83::sm83_sra()
 {}
 
-void SM83::asm_srl()
+void SM83::sm83_srl()
 {}
 
-void SM83::asm_stop()
+void SM83::sm83_stop()
 {}
 
-void SM83::asm_sub()
+void SM83::sm83_sub()
 {}
 
-void SM83::asm_swap()
+void SM83::sm83_swap()
 {}
 
-void SM83::asm_xor()
+void SM83::sm83_xor()
 {}
 
 #pragma endregion
