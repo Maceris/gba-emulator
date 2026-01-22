2ca5058ba3c23a75af5d85f432d22ad763447cb4
David Burks <chesburks@gmail.com>
2026-01-21 21:13:02 -0500

    Add 8 and 16 bit wires

--- emulator/include/board/wire.h
+++ emulator/include/board/wire.h
@@ -5,18 +5,27 @@
 namespace emulator {
 	using Wire1 = uint8_t;
 	using Wire2 = uint8_t;
+	using Wire8 = uint8_t;
+	using Wire16 = uint16_t;
 	using Wire32 = uint32_t;
 
 	void set_bit(Wire1* wire, bool on);
 	void set_bit(Wire2* wire, int bit, bool on);
+	void set_bit(Wire8* wire, int bit, bool on);
+	void set_bit(Wire16* wire, int bit, bool on);
 	void set_bit(Wire32* wire, int bit, bool on);
 
 	bool get_bit(Wire1* wire);
 	bool get_bit(Wire2* wire, int bit);
+	bool get_bit(Wire8* wire, int bit);
+	bool get_bit(Wire16* wire, int bit);
 	bool get_bit(Wire32* wire, int bit);
 
+// Annotation used to indicate signal coming into a component, does nothing.
 #define WIRE_INCOMING
+// Annotation used to indicate signal coming out of a component, does nothing.
 #define WIRE_OUTGOING
+// Annotation used to indicate signal internal to a component, does nothing.
 #define WIRE_INTERNAL
 
 }
--- emulator/src/board/wire.cpp
+++ emulator/src/board/wire.cpp
@@ -28,6 +28,32 @@ namespace emulator {
 		}
 	}
 
+	void set_bit(Wire8* wire, int bit, bool on) {
+		LOG_ASSERT(wire != nullptr);
+		LOG_ASSERT(bit >= 0 && bit <= 7
+			&& "Bit must be between 0 and 7, inclusive");
+
+		if (on) {
+			*wire |= 1 << bit;
+		}
+		else {
+			*wire &= ~(1 << bit);
+		}
+	}
+
+	void set_bit(Wire16* wire, int bit, bool on) {
+		LOG_ASSERT(wire != nullptr);
+		LOG_ASSERT(bit >= 0 && bit <= 15
+			&& "Bit must be between 0 and 15, inclusive");
+
+		if (on) {
+			*wire |= 1 << bit;
+		}
+		else {
+			*wire &= ~(1 << bit);
+		}
+	}
+
 	void set_bit(Wire32* wire, int bit, bool on) {
 		LOG_ASSERT(wire != nullptr);
 		LOG_ASSERT(bit >= 0 && bit <= 31
@@ -50,6 +76,20 @@ namespace emulator {
 		LOG_ASSERT(wire != nullptr);
 		LOG_ASSERT(bit >= 0 && bit <= 1
 			&& "Bit must be between 0 and 1, inclusive");
+		return ((*wire >> bit) & 0b1) != 0;
+	}
+
+	bool get_bit(Wire8* wire, int bit) {
+		LOG_ASSERT(wire != nullptr);
+		LOG_ASSERT(bit >= 0 && bit <= 7
+			&& "Bit must be between 0 and 7, inclusive");
+		return ((*wire >> bit) & 0b1) != 0;
+	}
+
+	bool get_bit(Wire16* wire, int bit) {
+		LOG_ASSERT(wire != nullptr);
+		LOG_ASSERT(bit >= 0 && bit <= 15
+			&& "Bit must be between 0 and 15, inclusive");
 		return ((*wire >> bit) & 0b1) != 0;
 	}
 
